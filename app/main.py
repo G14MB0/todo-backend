00607398f6bd0e13a851886910a130c691b707ea
G14MB0 <castaldini.gianmaria@gmail.com>
2024-05-03 16:06:35 +0200

    add static serve of dist and docs

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@
 uvicorn app.main:app --reload #start the server without the main.py file
 '''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, staticfiles, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import todo, user, auth
+
+from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from app import models
 from app.database import engine
@@ -70,10 +73,36 @@ app.include_router(auth.router, prefix="/api/v1")
 app.include_router(todo.router, prefix="/api/v1")
 
 
+# This will mount the docs as a static file. get them at "/mkdocs" endpoint
+app.mount("/mkdocs", staticfiles.StaticFiles(directory="./site", html=True), name="mkdocs")
 
-@app.get("/")
-def root():
-    return {"message": "This are the APIs. Go to ./docs to see documentations"}
+# This mount a static file that will be served at ./filename (so for example in order to open it, call .../index.html)
+# Since I mounted a Vue-Vite app with base = ".", I MUST mount at "/" thus Vite app's relative import still works correctly
+# Mounting at root path will also hide the index.html from the url path when called
+app.mount("/", staticfiles.StaticFiles(directory="./dist", html=True), name="dist")
+
+
+## Creat this custom midderlware class to handle 404 responses. I this case I force the app
+# To redirect at root path (static website) at all the not found path. This is used specifically
+# in this particula care and just as a demontration.
+class CustomMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        if response.status_code == 404:
+            # Redirect to root if a 404 response is generated
+            return RedirectResponse(url="/")
+        return response
+
+# Add custom middleware to the application
+app.add_middleware(CustomMiddleware)
+
+
+
+## Hide this since overwritten by the app.mount
+# @app.get("/")
+# def root():
+#     return {"message": "This are the APIs. Go to ./docs to see documentations"}
+
 
 
 @app.get("/health")
